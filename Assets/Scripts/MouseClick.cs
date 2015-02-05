34c9bac2b796d683f3fa39b29acd9b0928e6ca25
Adam Stack <bigstack80@knights.ucf.edu>
2015-02-04 23:21:09 -0500

    Created a basic player object and gave him basic movement. Try running the scene and click on the different cubes of grass.

---
+++ Assets/Scripts/MouseClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MouseClick : MonoBehaviour {
+
+	Transform player;
+	private Vector3 blockPossition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		blockPossition = gameObject.transform.position;
+		blockPossition.y += 1;
+		player.position = blockPossition;
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float moveSpeed;
+	private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// 1
+		Vector3 currentPosition = transform.position;
+		// 2
+		if( Input.GetButton("Fire1") ) {
+			// 3
+			Vector3 moveToward = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+			// 4
+			moveDirection = moveToward - currentPosition;
+			moveDirection.z = 0; 
+			moveDirection.Normalize();
+		}
+
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
+	}
+}
