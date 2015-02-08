a7e7530d686413ce5d945fb8864b79d6995f0021
Adam Stack <bigstack80@knights.ucf.edu>
2015-02-08 11:56:22 -0500

    I added the movement script to the character and also attached a sprite just for fun.

--- Assets/Scripts/MouseClick.cs
+++ Assets/Scripts/MouseClick.cs
@@ -6,6 +6,8 @@ public class MouseClick : MonoBehaviour {
 
 	Transform player;
 	private Vector3 blockPossition;
+	private float distx = .0f;
+	private float distz = .0f;
 
 	// Use this for initialization
 	void Start () {
@@ -24,6 +26,13 @@ public class MouseClick : MonoBehaviour {
 	void OnMouseDown() {
 		blockPossition = gameObject.transform.position;
 		blockPossition.y += 1;
+
+		distx = player.position.x - blockPossition.x;
+		distz = player.position.z - blockPossition.z;
 		player.position = blockPossition;
+	}
+
+	void OnMouseOver() {
+
 	}
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -14,19 +14,5 @@ public class PlayerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		// 1
-		Vector3 currentPosition = transform.position;
-		// 2
-		if( Input.GetButton("Fire1") ) {
-			// 3
-			Vector3 moveToward = Camera.main.ScreenToWorldPoint( Input.mousePosition );
-			// 4
-			moveDirection = moveToward - currentPosition;
-			moveDirection.z = 0; 
-			moveDirection.Normalize();
-		}
-
-		Vector3 target = moveDirection * moveSpeed + currentPosition;
-		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
 	}
 }
