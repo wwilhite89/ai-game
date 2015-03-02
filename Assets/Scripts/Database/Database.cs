c7fc827a7552c509bb16bd684978b33fffff91a3
Jason Braswell <jason.braswell@knights.ucf.edu>
2015-03-02 12:23:46 -0500

    Fixed plugin pro error
    - Fixed issue where pro license error would occur when calling sqlite
    dll.
    - Added db to git-ignore.
    - Remove extraneous files for other OS (for now).

--- Assets/Scripts/Database/Database.cs
+++ Assets/Scripts/Database/Database.cs
@@ -28,35 +28,21 @@ public class Database : MonoBehaviour {
 
         yield return new WaitForSeconds(0);
     }
-
+    
     private void createCharacters(SQLiteConnection c)
     {
        
         //var characters = c.CreateTable<Character>();
 
-        c.Insert(new Character { 
+        /*c.Insert(new Character { 
             Name = "The Hound",
             HP = 1000
-        });
+        });*/
 
         var q = c.Table<Character>().Where(x => x.Name == "The Hound");
 
         foreach (var r in q)
             Debug.Log(r.Name + " found!");
-
-
-        /*
-        var cmd = c.CreateCommand("create table IF NOT EXISTS Character(Name varchar(256), HP int, " +
-                "ATT int, DEF int, EVA int, CRIT int, MOV int, House varchar(256));");
-        */
-
-           /* using (var tran = c.BeginTransaction())
-            {
-                cmd.CommandText = string.Format("INSERT INTO Character VALUES ({0},{1},{2},{3},{4},{5},{6},{7})", 
-                    "The Hound", 1000, 500, 350, 25, 5, 6, "Lannister");
-                tran.Commit();
-            }
-        }*/
     }
 
 
