26f257bd8fec3550594154a5364de45d9b75077d
Jason Braswell <jason.braswell@knights.ucf.edu>
2015-03-29 17:39:08 -0400

    Updates to db

--- Assets/Scripts/Database/Database.cs
+++ Assets/Scripts/Database/Database.cs
@@ -3,15 +3,18 @@ using System;
 using System.Collections;
 using System.Linq;
 using SQLite4Unity3d;
+using System.Collections.Generic;
 
 public class Database : MonoBehaviour {
 
     
     private readonly string connectionString = "Game.db";
+    public bool isInitialized { get; private set; }
 
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(this.setup());
+        this.isInitialized = false;
+        StartCoroutine(this.initializeDatabase());
 	}
 	
 	// Update is called once per frame
@@ -19,30 +22,82 @@ public class Database : MonoBehaviour {
 	
 	}
 
-    private IEnumerator setup() {
+    private IEnumerator initializeDatabase() {
 
         using (var c = new SQLiteConnection(connectionString, false))
         {
-            this.createCharacters(c);
+            c.BeginTransaction();
+            //c.Execute("IF EXISTS ..Character
+            // Create characters
+
+            //var droppage = c.DropTable<Character>();
+            //Debug.Log(droppage);
+            var table = c.Execute("", new object[]{});
+            Debug.Log(table); // check result
+            //c.InsertAll(getInitialCharacters(), false);
+
+            c.Commit();
         }
 
+        this.isInitialized = true;
         yield return new WaitForSeconds(0);
     }
     
-    private void createCharacters(SQLiteConnection c)
+    private IEnumerable<Character> getInitialCharacters()
     {
-       
-        //var characters = c.CreateTable<Character>();
+        var c = new List<Character>();
+        
+        #region Greyjoy
+        c.Add(new Character { Name = "Balon Greyjoy", House = Character.HouseName.GREYJOY});
+        c.Add(new Character { Name = "Theon Greyjoy", House = Character.HouseName.GREYJOY });
+        c.Add(new Character { Name = "Victarian Greyjoy", House = Character.HouseName.GREYJOY });
+        c.Add(new Character { Name = "Euron Crow's Eye", House = Character.HouseName.GREYJOY });
+        c.Add(new Character { Name = "Ashsa Greyjoy", House = Character.HouseName.GREYJOY });
+        c.Add(new Character { Name = "Dagmar Cleftjaw", House = Character.HouseName.GREYJOY });
+        c.Add(new Character { Name = "Aaron Damphair", House = Character.HouseName.GREYJOY });
+        #endregion
 
-        /*c.Insert(new Character { 
-            Name = "The Hound",
-            HP = 1000
-        });*/
+        #region Stark
+        c.Add(new Character { Name = "Eddard Stark", House = Character.HouseName.STARK });
+        c.Add(new Character { Name = "Robb Stark", House = Character.HouseName.STARK });
+        c.Add(new Character { Name = "Catelyn Stark", House = Character.HouseName.STARK });
+        c.Add(new Character { Name = "Roose Bolton", House = Character.HouseName.STARK });
+        c.Add(new Character { Name = "Greation Umber", House = Character.HouseName.STARK });
+        c.Add(new Character { Name = "The Blackfish", House = Character.HouseName.STARK });
+        c.Add(new Character { Name = "Ser Rodrick Cassel", House = Character.HouseName.STARK });
+        #endregion
 
-        var q = c.Table<Character>().Where(x => x.Name == "The Hound");
+        #region Lannister
 
-        foreach (var r in q)
-            Debug.Log(r.Name + " found!");
+        c.Add(new Character { Name = "Jaime Lannister", House = Character.HouseName.LANNISTER });
+        c.Add(new Character { Name = "The Hound", House = Character.HouseName.LANNISTER });
+        c.Add(new Character { Name = "The Mountain", House = Character.HouseName.LANNISTER });
+        c.Add(new Character { Name = "Tywin Lannister", House = Character.HouseName.LANNISTER });
+        c.Add(new Character { Name = "Cersei Lannister", House = Character.HouseName.LANNISTER });
+        c.Add(new Character { Name = "Ser Kevan Lannister", House = Character.HouseName.LANNISTER });
+        c.Add(new Character { Name = "Tyrion Lannister", House = Character.HouseName.LANNISTER });
+
+        #endregion
+
+        #region MARTELL
+
+        c.Add(new Character { Name = "Randell Tarli", House = Character.HouseName.MARTELL });
+        
+        #endregion
+
+        #region TYRELL
+
+        c.Add(new Character { Name = "Eddard Stark", House = Character.HouseName.TYRELL });
+
+        #endregion
+        
+        #region BARATHEON
+
+        c.Add(new Character { Name = "Eddard Stark", House = Character.HouseName.BARATHEON });
+
+        #endregion
+
+        return c.AsEnumerable();
     }
 
 
--- Assets/Scripts/Database/Entities/Character.cs
+++ Assets/Scripts/Database/Entities/Character.cs
@@ -1,24 +1,51 @@
 ﻿using System;
 using System.Collections;
 using System.Linq;
-
+using SQLite4Unity3d;
 
 class Character
 {
+
+    #region Enumerations
+    public enum HouseName
+    {
+        STARK,
+        GREYJOY,
+        LANNISTER,
+        MARTELL,
+        TYRELL,
+        BARATHEON,
+        UNAFFILIATED,
+    }
+    #endregion
+
+    #region Fields
+
+    [PrimaryKey]
     public string Name { get; set; }
     public int HP { get; set; }
     public int ATT { get; set; }
     public int DEF { get; set; }
-    public int EVA { get; set; }
-    public int CRIT { get; set; }
+    public float EVA { get; set; }
+    public float CRIT { get; set; }
     public int MOV { get; set; }
-    // public virtual House House { get; private set; }
+    public HouseName House { get; set; }
+
+    #endregion
+
+    #region Constructors
 
     public Character() {
-        this.loadCharacter();
+        // Default stats
+        this.HP = 1000;
+        this.ATT = 250;
+        this.DEF = 100;
+        this.EVA = 0.5f;
+        this.CRIT = 0.01f;
+        this.MOV = 6;
+        this.House = Character.HouseName.UNAFFILIATED;
     }
 
-    private bool loadCharacter() {
-        return true;
-    }
+    #endregion
+
 }
