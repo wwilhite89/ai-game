b6cf04f619c060267e48991788d238600f9e7a54
Wil Wilhite <wwilhite89@gmail.com>
2015-04-13 18:38:47 -0400

    The picture of the character is displayed on the character card UI element.

---
+++ Assets/Scripts/UI/ImageDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageDisplay : MonoBehaviour {
+	Image image;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.FindGameObjectWithTag("Selected");
+		image.sprite = player.GetComponentInChildren<SpriteRenderer>().sprite;
+		image.SetNativeSize();
+	}
+}
---
+++ Assets/Scripts/UI/NameDisplay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NameDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text>().text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject character = GameObject.FindGameObjectWithTag("Selected");
+		GetComponent<Text>().text = character.name;
+	}
+}
