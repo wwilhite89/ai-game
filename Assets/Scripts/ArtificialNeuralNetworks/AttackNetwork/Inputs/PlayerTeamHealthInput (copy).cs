4ecbc6c7854de26e765e313f1e3e54c882617c00
Adam Stack <bigstack80@knights.ucf.edu>
2015-04-15 13:16:49 -0400

    added inputs for the net

---
+++ Assets/Scripts/ArtificialNeuralNetworks/AttackNetwork/Inputs/PlayerTeamHealthInput (copy).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArtificialNeuralNetworks.Training;
+
+namespace ArtificialNeuralNetworks.AttackNetwork.Inputs
+{
+    public class PlayerTeamHealthInput : TrainingComponent
+    {
+
+		public PlayerTeamHealthInput(PlayerScript player, int cycleTime)
+            : base(player, cycleTime)
+        { }
+
+        protected override void UpdateTraining()
+        {
+            if (this.player != null)
+                this.newValue = this.player.GetStat(GameDB.Character.Stats.HP);
+        }
+
+    }
+}
