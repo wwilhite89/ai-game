ba04b6115e31d85624c67396ccf322956f6972cd
Wil Wilhite <wwilhite89@gmail.com>
2015-04-23 14:26:33 -0400

    More fixes for attack button. The attack button is a toggle that puts the character into attack status. It'll stay pressed even when you switch characters. You can only attack once per turn.

--- Assets/Scripts/UI/AttackButton.cs
+++ Assets/Scripts/UI/AttackButton.cs
@@ -8,6 +8,7 @@ using GameDB;
 public class AttackButton : MonoBehaviour {
     private LevelManager lvlMgr;
 	GameObject character;
+	CharacterController characterCtrl;
 	private bool engaged;
 	Button b;
 	ColorBlock normalColor;
@@ -17,29 +18,38 @@ public class AttackButton : MonoBehaviour {
         this.lvlMgr = GameObject.Find("Manager").GetComponent<LevelManager>();
 		this.character = null;
 		this.engaged = false;
-		this.b = gameObject.GetComponent<Button>();
+		this.b = this.gameObject.GetComponent<Button>();
 		this.normalColor = b.colors;
 	}
 
 	void Update() {
-		if(lvlMgr.ActiveCharacter != null) {
-			this.character = lvlMgr.ActiveCharacter;
+		if(lvlMgr.ActiveCharacter != null) { 
+			if(!lvlMgr.ActiveCharacter.Equals(character)) {
+				this.character = lvlMgr.ActiveCharacter;
+				this.characterCtrl = character.GetComponent<CharacterController>();
+			}
+			if(this.characterCtrl != null) {
+				if(this.characterCtrl.character.status == Character.Status.READY) {
+					this.ChangeColor(Color.white);
+					lvlMgr.resetCharColor();
+				} else if(this.characterCtrl.character.status == Character.Status.ATTACKING) {
+					this.ChangeColor(Color.red);
+				}
+			}
 		}
 	}
 
 	public void OnClick() {
 		if(this.engaged) {
 			this.engaged= false;
-			this.ChangeColor(Color.white);
-			lvlMgr.resetCharColor();
-		} else if(this.character != null) {
-			this.character.GetComponent<CharacterController>().character.status = Character.Status.ATTACKING;
-			this.ChangeColor(Color.red);
+			this.characterCtrl.character.status = Character.Status.READY;
+		} else if(this.character != null && !this.characterCtrl.HasAttacked) {
 			this.engaged = true;
+			this.characterCtrl.character.status = Character.Status.ATTACKING;
 		}
 	}
 
 	private void ChangeColor(Color c) {
-		this.gameObject.GetComponent<Button> ().image.color = c;
+		this.b.image.color = c;
 	}
 }
